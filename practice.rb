c0094f091843b89eec321ac49b6029afc1eb10fc
sgudur9 <18sgudur.stem@gmail.com>
2018-08-04 21:00:29 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -7,7 +7,7 @@ rate = gets.chomp.to_i
 puts "Over how many months will this loan continue?"
 months = gets.chomp.to_i
 
-principal = loanamount / months
-monthlypremium = principal(1+(rate/12))^(months)
+_principal = loanamount / months 
+_monthlypremium = principal(1+(rate/12))^(months) 
 
 puts "Your loan payback amount will be #{montlypremium} over #{months}."
