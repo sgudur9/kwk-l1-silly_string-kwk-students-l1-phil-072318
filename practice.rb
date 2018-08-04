1037f665f4fb2d5638e44b0a620444bdebc29883
sgudur9 <18sgudur.stem@gmail.com>
2018-08-04 20:53:20 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -8,4 +8,4 @@ puts "Over how many months will this loan continue?"
 months = gets.chomp.to_i
 
 principal = loanamount / months
-monthlypremium = principal(1+(rate/12))^(144*months)
+monthlypremium = principal(1+(rate/12))^(months)
